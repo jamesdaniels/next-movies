bd4ea269b203a5446beec41b12822bb4c7e4c296
James Daniels <jamesdaniels@google.com>
2022-04-12 13:34:40 -0400

    More demo

--- .firebase/next-cats/hosting/_next/static/chunks/pages/error-6b3d3cc468e0ccc9fcfa.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[510],{6380:function(n,u,r){"use strict";r.r(u);var t=r(5893),_=r(337);u.default=function(){return(0,t.jsx)(_.Z,{})}},8454:function(n,u,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/error",function(){return r(6380)}])}},function(n){n.O(0,[261,519,337,774,888,179],(function(){return u=8454,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
-//# sourceMappingURL=error-6b3d3cc468e0ccc9fcfa.js.map
